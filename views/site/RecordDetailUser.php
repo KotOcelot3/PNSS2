5780ec8fd31f7a4fd8cca537678d99fe03349cd0
KotOcelot3 <114900378+KotOcelot3@users.noreply.github.com>
2023-10-23 17:20:54 +0700

    Add files via upload

---
+++ views/site/RecordDetailUser.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="/public/css/subvision.css">
+<div class="sub">
+    <div class="sub_all">
+        <div class="sub_obj">
+            <h2>Название: <?= $record->title?></h2>
+            <a> диагноз:<?= $record->diagnosis ?></a>
+            <a>Пациент: <?= $record->patient?></a>
+            <a>Доктор:<?= $record->doctor?></a>
+        </div>
+    </div>
+</div>
