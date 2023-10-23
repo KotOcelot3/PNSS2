c40a529f8464250d12655c015e3c5577627f3e68
KotOcelot3 <114900378+KotOcelot3@users.noreply.github.com>
2023-10-23 17:08:45 +0700

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,26 +13,25 @@
     <nav class="nav">
         <a href="<?= app()->route->getUrl('/index') ?>">Главная</a>
         <?php
-        if (app()->auth::check()  && app()->auth::user()->role != 2):
+        if (app()->auth::check() && app()->auth::user()->role !== 1):
+            ?>
+            <a href="<?= app()->route->getUrl('/SpecializationList') ?>">Список специальностей</a>
+            <a href="<?= app()->route->getUrl('/AddRecord') ?>">Добавление Записи</a>
+            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
+        <?php
+        elseif (!app()->auth::check()):
             ?>
             <a href="<?= app()->route->getUrl('/login') ?>">Вход</a>
             <a href="<?= app()->route->getUrl('/RegisterUser') ?>">Регистрация</a>
-            <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
-            <img class="img_user" src="/pnss/public/assets/img/<?= app()->auth::user()->photo ?>" alt="">
-            <a href="<?= app()->route->getUrl('/SpecializationList') ?>">Список специальностей</a>
-            <a href="<?= app()->route->getUrl('/RegisterDoctor') ?>">Добавление Врача</a>
-            <a href="<?= app()->route->getUrl('/AddRecord') ?>">Добавление Записи</a>
-            <a href="<?= app()->route->getUrl('/DiagnosisList') ?>">Список диагнозов</a>
-            <a href="<?= app()->route->getUrl('/RecordList') ?>">Список записей к врачам</a>
         <?php
         else:
             ?>
-            <a href="<?= app()->route->getUrl('/DoctorList') ?>">Список врачей</a>
+            <a href="<?= app()->route->getUrl('/RecordList') ?>">Список записей к врачам</a>
+            <a href="<?= app()->route->getUrl('/DiagnosisList') ?>">Список диагнозов</a>
+            <a href="<?= app()->route->getUrl('/RegisterDoctor') ?>">Добавление Врача</a>
+            <a href="<?= app()->route->getUrl('/SpecializationList') ?>">Список специальностей</a>
+            <a href="<?= app()->route->getUrl('/AddRecord') ?>">Добавление Записи</a>
             <a href="<?= app()->route->getUrl('/logout') ?>">Выход (<?= app()->auth::user()->name ?>)</a>
-            <form  method="get" action="<?= app()->route->getUrl('/searchdb') ?>"  id="searchform">
-                <input  type="text" name="search">
-                <input class="search" type="submit" name="submit" value="Search">
-            </form>
         <?php
         endif;
         ?>
